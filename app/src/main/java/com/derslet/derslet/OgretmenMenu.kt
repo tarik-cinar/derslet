79888e3686c0576ccd87c6710385c6896d2475b9
Tarık Çınar <tarikcinar98@gmail.com>
2021-04-22 21:23:19 +0300

    OgretmenMenu

---
+++ app/src/main/java/com/derslet/derslet/OgretmenMenu.kt
@@ -0,0 +1,11 @@
+package com.derslet.derslet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OgretmenMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ogretmen_menu)
+    }
+}
