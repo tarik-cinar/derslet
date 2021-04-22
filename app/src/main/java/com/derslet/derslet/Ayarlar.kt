55138ec24f48b74ee6062739f2f564abcf40393a
Eren Erdoğan <dactarium@outlook.com>
2021-04-22 19:51:11 +0300

    ayarlar tasarımı (yarım)
    Kullanıcı bilgileri ve renk ayarı kısmı ayarlandı.
    
    Co-Authored-By: Tarık Çınar <77357220+tarik-cinar@users.noreply.github.com>

---
+++ app/src/main/java/com/derslet/derslet/Ayarlar.kt
@@ -0,0 +1,12 @@
+package com.derslet.derslet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ayarlar : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ayarlar)
+    }
+}
