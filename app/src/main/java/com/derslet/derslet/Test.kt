1e5275a7de4e2198ed7759f075290e7478fe0f43
Tarık Çınar <tarikcinar98@gmail.com>
2021-04-24 23:07:10 +0300

    Genel Düzenleme v5

--- app/src/main/java/com/derslet/derslet/Test.kt
+++ app/src/main/java/com/derslet/derslet/Test.kt
@@ -1,11 +1,38 @@
 package com.derslet.derslet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
+import java.util.*
 
 class Test : AppCompatActivity() {
+
+    var menu: TextInputLayout? = null
+    var acliste: AutoCompleteTextView? = null
+
+    var arrayList_season: ArrayList<String>? = null
+    var arrayAdapter_season: ArrayAdapter<String>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
+
+        menu = findViewById<View>(R.id.ders_secim) as TextInputLayout
+        acliste = findViewById<View>(R.id.ic_liste) as AutoCompleteTextView
+
+        arrayList_season = ArrayList()
+        arrayList_season!!.add("Amına")
+        arrayList_season!!.add("Koyim")
+        arrayList_season!!.add("Android")
+
+        arrayAdapter_season = ArrayAdapter(
+            applicationContext, R.layout.acilir_menu,
+            arrayList_season!!
+        )
+        acliste!!.setAdapter(arrayAdapter_season)
+        acliste!!.threshold = 1
     }
 }
